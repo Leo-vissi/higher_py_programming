783e704a324ea0b33a0858f05a77a0f93f0916ab
ahmed sayf <ahmlot92@gmail.com>
2023-08-13 16:24:25 -0700

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+
 a = 1
 b = 2
 
-while __name__ == "__main__":
+if __name__ == "__main__":
     from add_0 import add
 
-print('{:d} + {:d} = {:d}}'.format(a, b, add(a, b)))
+print('{} + {} = {}'.format(a, b, add(a, b)))
