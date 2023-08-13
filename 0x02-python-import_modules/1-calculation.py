8187fc76cede705e55d78aac0f6cb3a137b28a55
ahmed sayf <ahmlot92@gmail.com>
2023-08-13 16:49:29 -0700

    task 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+a, b = 10, 5
+
+#this code for calculat the value a and b in stor add, sub, mul, div 
+
+add(a, b) = a + b
+sub(a, b) = a - b
+mul(a, b) = a * b
+div(a, b) = a / b
+
+# this part for print the stor in func calculat 
+
+print("{} + {} = {}".format(a, b, add(a, b))
+print("{} - {} = {}".format(a, b, sub(a, b))
+print("{} * {} = {}".format(a, b, mul(a, b))
+print("{} / {} = {}".format(a, b, div(a, b))
