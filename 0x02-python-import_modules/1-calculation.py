8ac7a94b7544231e6c720533c9f9715c7ac1fda2
ahmed sayf <ahmlot92@gmail.com>
2023-08-13 17:31:58 -0700

    task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,9 +3,19 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
 
-a, b = 10, 5
+a = 10
+b = 5
 
-print('{} + {} = {}'.format(a, b, add(a, b)))
-print('{} - {} = {}'.format(a, b, sub(a, b)))
-print('{} * {} = {}'.format(a, b, mul(a, b)))
-print('{} / {} = {}'.format(a, b, div(a, b)))
+#this code for calculat the value a and b in stor add, sub, mul, div
+
+add(a, b) == a + b
+sub(a, b) == a - b
+mul(a, b) == a * b
+div(a, b) == a / b
+
+# this part for print the stor in func calculat
+
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
